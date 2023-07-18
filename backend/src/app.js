dc008bd2d0b94ab7828643f84cd6572f81bd0f9f
[AI7/2반] 손태린 <elice.str@gmail.com>
2023-07-19 01:41:11 +0900

    :construction: Working: 유저 MVP #IPZY-13

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,7 +1,20 @@
 import express from 'express';
+import cors from 'cors';
+import { userRouter } from './routers/userRouter';
+import { errorMiddleware } from './middlewares/errorMiddleware';
+
 const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
   res.send('Hello, Team02');
 });
+
+app.use([userRouter]);
+
+app.use(errorMiddleware);
+
 export { app };
