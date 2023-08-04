944c4c8f47b199242e786bdee6fb794b28c9dc83
[AI7/2반] 손태린 <elice.str@gmail.com>
2023-08-04 10:50:00 +0900

    :bug: Fix: API response 데이터 변수명 수정

--- frontend/src/components/main/myrecipe/MyRecipeForm.js
+++ frontend/src/components/main/myrecipe/MyRecipeForm.js
@@ -10,55 +10,54 @@ import {
   TagBoxBlock,
   TagForm,
   TagListBlock,
-} from '../create/AiComponents'
+} from '../create/AiComponents';
 import palette from '../../../lib/styles/palette';
 
 const SubInfo = styled.div`
-  margin-top : 1rem;
+  margin-top: 1rem;
   color: ${palette.gray[6]};
   display: flex;
   justify-content: end;
   margin-bottom: 0.5rem;
   span + span:before {
-    color : ${palette.gray[5]};
-    padding-left : 0.25rem;
+    color: ${palette.gray[5]};
+    padding-left: 0.25rem;
     padding-right: 0.25rem;
     content: '\\B7';
   }
-`
+`;
 const Tags = styled.div`
-  margin-top : 1rem;
+  margin-top: 1rem;
   display: flex;
   justify-content: center;
   .tag {
     display: inline-block;
-    color : ${palette.cyan[9]};
+    color: ${palette.cyan[9]};
     text-decoration: none;
     margin-right: 0.5rem;
     font-size: 1.3rem;
     font-weight: bold;
     &:hover {
-      color : ${palette.cyan[6]};
+      color: ${palette.cyan[6]};
     }
   }
-`
+`;
 
-
-const MyRecipeForm = ({ post, error, loading}) => {
-
-if(error){
-  if(error.response && error.response.status === 404) {
-    return <CreateAireturnBlock>존재하지 않는 포스트 입니다.</CreateAireturnBlock>
+const MyRecipeForm = ({ post, error, loading }) => {
+  if (error) {
+    if (error.response && error.response.status === 404) {
+      return <CreateAireturnBlock>존재하지 않는 포스트 입니다.</CreateAireturnBlock>;
+    }
+    return <CreateAireturnBlock>오류 발생</CreateAireturnBlock>;
   }
-  return <CreateAireturnBlock>오류 발생</CreateAireturnBlock>
-}
 
-if (loading || !post){
-  return null;
-}
+  if (loading || !post) {
+    return null;
+  }
 
-const {foodname, ingredients, recipe, createdAt} = post;
+  const { foodname, ingredients, recipe, createdAt } = post.recipe;
 
+  const { comment } = post;
 
   return (
     <>
@@ -69,26 +68,30 @@ const {foodname, ingredients, recipe, createdAt} = post;
         </ImgUpload>
 
         <AiReturnbox>
-          <SubInfo><span><b>이름</b></span><span>{new Date(createdAt).toLocaleDateString()}</span></SubInfo>
-          <h3 contentEditable="true">
-          요리이름 : {foodname}</h3>
+          <SubInfo>
+            <span>
+              <b>이름</b>
+            </span>
+            <span>{new Date(createdAt).toLocaleDateString()}</span>
+          </SubInfo>
+          <h3 contentEditable="true">요리이름 : {foodname}</h3>
           <label className="divbox">
             <div className="one" contentEditable="true">
-            재료 : {ingredients}
+              재료 : {ingredients}
             </div>
             <div className="two" contentEditable="true">
-            레시피 : {recipe}
+              레시피 : {recipe}
             </div>
           </label>
 
-        <Tags>
-          <div className='tag'>#태그1</div>
-          <div className='tag'>#태그2</div>
-          <div className='tag'>#태그3</div>
-        </Tags>
+          <Tags>
+            <div className="tag">#태그1</div>
+            <div className="tag">#태그2</div>
+            <div className="tag">#태그3</div>
+          </Tags>
 
           <div className="twobtn">
-            <Button >전체 레시피 삭제</Button>
+            <Button>전체 레시피 삭제</Button>
           </div>
         </AiReturnbox>
       </CreateAireturnBlock>
--- frontend/src/lib/api/posts.js
+++ frontend/src/lib/api/posts.js
@@ -24,16 +24,12 @@ export const savePost = async ({ foodname, ingredients, recipe, tags, aiRecipeId
   );
 };
 
-export const readPost = async ({recipeId,token }) => {
-  return await client.get(`/recipes/${recipeId}`,
-  {recipeId},
-  {
+export const readPost = async ({ recipeId, token }) => {
+  return await client.get(`/recipes/${recipeId}`, {
     headers: {
       Authorization: `Bearer ${token}`,
     },
-  },
-  
-  )
-}
+  });
+};
 
 ///recipes/:recipeId
