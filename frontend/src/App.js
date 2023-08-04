b64d684f0be3dc723ec24a3d00d59bce6c6b1e23
[AI7/2반] 조성진 <tjdwls1286@gmail.com>
2023-08-04 19:45:10 +0900

    레시피 목록 구현중

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,13 +4,14 @@ import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
 import MainPage from './pages/MainPage';
 import CreateRecipe from './pages/Create/CreateRecipe';
-import MyRecipe from './pages/MyRecipe/MyRecipe';
+import MyRecipePage from './pages/MyRecipe/MyRecipePage';
 import RecommendRecipe from './pages/RecommendRecipe';
 import ReviewRecipe from './pages/ReviewRecipe';
 import CreateAiReturn from './pages/Create/CreateAiReturn';
 import AccountPage from './pages/AccountPage';
+
 import LeavePage from './pages/LeavePage';
-import MyRecipeList from './pages/MyRecipe/MyRecipeList';
+import MyRecipeListPage from './pages/MyRecipe/MyRecipeListPage';
 
 function App() {
   return (
@@ -22,12 +23,12 @@ function App() {
       <Route path="/createAi" element={<CreateAiReturn />} />
 
       <Route path="/:email">
-        <Route index element={<MyRecipeList />} />
-        <Route path=":postId" element={<MyRecipe />} />
+        <Route index element={<MyRecipeListPage />} />
+        <Route path=":postId" element={<MyRecipePage />} />
       </Route>
 
       {/* 임시 my레시피 주소 */}
-      <Route path="/myrecipe" element={<MyRecipe />} />
+      <Route path="/myrecipe" element={<MyRecipePage />} />
       {/* ㅡㅡㅡㅡㅡㅡ */}
 
       <Route path="/recommend" element={<RecommendRecipe />} />
--- frontend/src/components/main/create/ImgComponent.js
+++ frontend/src/components/main/create/ImgComponent.js
@@ -1,90 +1,89 @@
-import styled from "styled-components";
-import palette from "../../../lib/styles/palette";
-import {useEffect, useState,useRef} from 'react';
+import styled from 'styled-components';
+import palette from '../../../lib/styles/palette';
+import { useEffect, useState, useRef } from 'react';
 import axios from 'axios';
 
-
-
-
 const Wrapper = styled.div`
-      display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    .img-wrapper{
-        margin: 50px 0 20px 0;
-        img{
-            width: 200px;
-            height: 200px;
-        }
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  .img-wrapper {
+    margin: 50px 0 20px 0;
+    img {
+      width: 200px;
+      height: 200px;
     }
-    .upload-button{
-        button{
-            margin: 0 5px;
-        }
+  }
+  .upload-button {
+    button {
+      margin: 0 5px;
     }
-`
+  }
+`;
 
-const Uploader = (e) => {
+const Uploader = e => {
+  const inputRef = useRef();
+  const [image, setImage] = useState({
+    image_file: '',
+    preview_URL: 'logo.png',
+  });
 
-    const inputRef = useRef()
-    const [image, setImage] = useState({
-        image_file: "",
-        preview_URL : "logo.png"
+  const saveImage = e => {
+    e.preventDefault();
+    if (e.target.files[0]) {
+      URL.revokeObjectURL(image.preview_URL);
+      const preview_URL = URL.createObjectURL(e.target.files[0]);
+      setImage(() => ({
+        image_file: e.target.files[0],
+        preview_URL: preview_URL,
+      }));
+    }
+  };
+
+  const deleteImge = () => {
+    URL.revokeObjectURL(image.preview_URL);
+    setImage({
+      image_file: '',
+      preview_URL: 'logo.png',
     });
-   
+  };
 
+  useEffect(() => {
+    return () => {
+      URL.revokeObjectURL(image.preview_URL);
+    };
+  }, []);
 
-    const saveImage = (e) => {
-        e.preventDefault();
-        if(e.target.files[0]){
-            URL.revokeObjectURL(image.preview_URL);
-            const preview_URL = URL.createObjectURL(e.target.files[0])
-            setImage(()=>({
-                image_file: e.target.files[0],
-                preview_URL : preview_URL
-            }))
-
-        }
+  const sendImageToServer = async () => {
+    if (image.image_file) {
+      const formData = new FormData();
+      formData.append('imgfile', image.image_file);
+      await axios.post('경로', formData);
+      alert('서버에 등록이 완료되었습니다.');
+      setImage({
+        image_file: '',
+        preview_URL: 'logo.png',
+      });
+    } else {
+      alert('사진을 등록하세요!');
     }
+  };
 
-    const deleteImge = () => {
-        URL.revokeObjectURL(image.preview_URL)
-        setImage({
-            image_file: "",
-            preview_URL : "logo.png"
-        })
-    }
-
-    useEffect(()=>{
-        return()=> {
-            URL.revokeObjectURL(image.preview_URL)
-        }
-    },[])
-
-    const sendImageToServer = async () => {
-        if(image.image_file){
-            const formData = new FormData()
-            formData.append('imgfile', image.image_file)
-            await axios.post('경로',formData);
-            alert('서버에 등록이 완료되었습니다.')
-            setImage({
-                image_file : "",
-                preview_URL: "logo.png"
-            })
-        } else {
-            alert("사진을 등록하세요!")
-        }
-    }
-
-    return(
-       <Wrapper>
-            <input type="file" accept="image/jpg, image/jpeg, image/png" onChange={saveImage} onClick={(e)=>{e.target.value = null}}
-                ref={inputRef} style={{display : "none"}} />
-
-       </Wrapper>
-    )
-
-}
+  return (
+    <Wrapper>
+      <input
+        type="file"
+        accept="image/jpg, image/jpeg, image/png"
+        onChange={saveImage}
+        onClick={e => {
+          e.target.value = null;
+        }}
+        ref={inputRef}
+        style={{ display: 'none' }}
+      />
+    </Wrapper>
+  );
+};
 
 export default Uploader;
